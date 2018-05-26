c978ac4626c0c80b3da676d5f861f9215dceb5d4
Thomas T <contact@kurianinc.us>
2018-05-26 18:10:02 +0000

    Updated message

--- src/main/java/org/examples/java/App.java
+++ src/main/java/org/examples/java/App.java
@@ -5,6 +5,6 @@ package org.examples.java;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! from JT");
+        System.out.println("Hello World! from JT Take2:w");
     }
 }
