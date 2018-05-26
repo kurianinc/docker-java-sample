f018d1082404b81f28263f2b9dfe8283d6dde0be
Thomas T <contact@kurianinc.us>
2018-05-26 17:58:52 +0000

    Revert "Updated message"
    This reverts commit 6663ab8cc123283ae054c277bab37c43eb08212d.

--- src/main/java/org/examples/java/App.java
+++ src/main/java/org/examples/java/App.java
@@ -6,6 +6,5 @@ package org.examples.java;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-	System.out.println("New Messages.");
     }
 }
