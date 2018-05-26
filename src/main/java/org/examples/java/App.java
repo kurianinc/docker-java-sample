5736f364284c70dc6748e34d6bbc64bbe0479b6d
Thomas T <contact@kurianinc.us>
2018-05-26 18:30:38 +0000

    Updated thanks message

--- src/main/java/org/examples/java/App.java
+++ src/main/java/org/examples/java/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("\nFrom Pedro Sanchez A.!");
+        System.out.println("\n! Thanks !!!");
     }
 }
