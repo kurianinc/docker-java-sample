4e65188225aef28cd2e422929e6349ef2b5b2219
Thomas T <contact@kurianinc.us>
2018-05-26 19:48:48 +0000

    Updated message

--- src/main/java/org/examples/java/App.java
+++ src/main/java/org/examples/java/App.java
@@ -5,6 +5,7 @@ package org.examples.java;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! from JT Take2:w");
+        System.out.println("Hello World! from JT Take2 and from Jenkins Lab");
+
     }
 }
