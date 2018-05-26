b14987e7632b1e2570d20c1cf4921e6b2bdab35f
Thomas T <contact@kurianinc.us>
2018-05-26 18:06:42 +0000

    Updated message

--- src/main/java/org/examples/java/App.java
+++ src/main/java/org/examples/java/App.java
@@ -1,10 +1,10 @@
 package org.examples.java;
 
 /**
- * Hello world!
+ * Hello world007!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World007!");
     }
 }
