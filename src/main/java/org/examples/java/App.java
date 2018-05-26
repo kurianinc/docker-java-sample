ffc0aa7d7181a917207a48660798dfe1bda16699
Thomas T <contact@kurianinc.us>
2018-05-26 20:01:53 +0000

    Live

--- src/main/java/org/examples/java/App.java
+++ src/main/java/org/examples/java/App.java
@@ -5,6 +5,6 @@ package org.examples.java;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Real live World!");
+        System.out.println("Hello LIve World!");
     }
 }
