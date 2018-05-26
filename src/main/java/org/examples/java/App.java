d103281b1f695349b46507d407f7f9366b1ba9dc
Thomas T <contact@kurianinc.us>
2018-05-26 20:52:32 +0000

    Updated message to fix syntax

--- src/main/java/org/examples/java/App.java
+++ src/main/java/org/examples/java/App.java
@@ -5,5 +5,5 @@ package org.examples.java;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!, Baiju Joseph Thalupadath") }
+        System.out.println("Hello World!, Baiju Joseph Thalupadath";) }
 }
