c3e333e55b9d7244449d4637614a7bab3527dc28
Thomas T <contact@kurianinc.us>
2018-05-26 22:33:44 +0000

    Updated message

--- src/main/java/org/examples/java/App.java
+++ src/main/java/org/examples/java/App.java
@@ -5,6 +5,5 @@ package org.examples.java;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkis Devender!") }
-        // System.out.println("Hello Jenkis Devender!"); }
+        System.out.println("Hello 2nd Jenkis Devender!") }
 }
