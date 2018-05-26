ca748c05d688779377e045dedf2061b742ab799d
Thomas T <contact@kurianinc.us>
2018-05-26 20:44:48 +0000

    update bug fix

--- src/main/java/org/examples/java/App.java
+++ src/main/java/org/examples/java/App.java
@@ -5,6 +5,6 @@ package org.examples.java;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!! 123")
+        System.out.println("Hello World!! 123");
     }
 }
